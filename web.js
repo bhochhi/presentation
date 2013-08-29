8938b8d21b639fd43ff7343da13a1080e94d3f0b
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2013-08-28 21:34:52 -0500

    adding complete presentation

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var fs  = require('fs');
 var app = express();
-app.use("/design", express.static(__dirname + '/design'));
+app.use("/impress_files", express.static(__dirname + '/impress_files'));
+app.use("/bespoke_files", express.static(__dirname + '/bespoke_files'));
 app.use(app.router);
 app.get('/', function(request, response) {
-  response.send((fs.readFileSync("./index.html")).toString());
+  response.send((fs.readFileSync("./impress.html")).toString());
+  });
+  app.get('/bespoke', function(request, response) {
+  response.send((fs.readFileSync("./bespoke.html")).toString());
   });
 
   var port = process.env.PORT || 5000;
